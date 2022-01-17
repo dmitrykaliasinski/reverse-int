fe0341db266c1e797782b8673d8e26e7e7d60ecf
dmitrykaliasinski <d.k.akadiman@gmail.com>
2022-01-17 18:16:40 +0300

    feat: made working function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let number = Math.abs(n) + "";
+    let rev = "";
+    for (let i = number.length - 1; i >= 0; i--) {
+        rev += number[i];
+    }
+    return Number(rev);
+};
